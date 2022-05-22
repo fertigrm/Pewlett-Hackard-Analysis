289b898b96ddff917ad4ebc8318c228d93e97235
Ryan <fertigryan66@gmail.com>
2022-05-22 18:27:25 -0500

    updating after error

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -17,13 +17,13 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFRENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFRENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
@@ -31,24 +31,24 @@ CREATE TABLE salaries (
 	salary INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFRENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
+	title INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFRENCES employees (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-CREATE TABLE dept_emp
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFRENCES employees (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 SELECT * FROM departments;
